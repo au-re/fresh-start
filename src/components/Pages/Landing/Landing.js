fb1d90d5ac588d0021c2b780e21ceb318dff713f
Aurelien Hontabat <aurelien.hontabat@fcc.chalmers.se>
2018-05-17 18:03:37 +0200

    :sparkles: (boilerplate) improve boilerplate demo

--- src/components/Pages/Landing/Landing.js
+++ src/components/Pages/Landing/Landing.js
@@ -1,17 +1,53 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 import Button from "../../Button/Button";
 
+const LandingTitle = styled.h1`
+  color: white;
+  text-align: center;
+  text-transform: uppercase;
+`;
+
+const LandingLink = styled(Link) `
+  color: white;
+  font-size: 1rem;
+
+  :hover {
+    text-decoration: none;
+    color: white;
+  }
+`;
+
+const LandingHeader = styled.div`
+  padding: 2rem;
+
+
+`;
+
 class Landing extends Component {
+
+  static Link = LandingLink;
+  static Title = LandingTitle;
+  static Header = LandingHeader;
+
   render() {
     const { className } = this.props;
 
     return (
       <div className={className}>
-        landing page
-        <Button>hello world</Button>
+        <div>
+          <LandingLink to="todos">todos</LandingLink>
+        </div>
+        <LandingTitle>I'm just a Boilerplate</LandingTitle>
       </div>);
   }
 }
 
-export default Landing;
+export default styled(Landing) `
+  background: tomato;
+  color: white;
+  height: 100%;
+  display: grid;
+`;
--- src/components/Pages/Landing/Landing.stories.js
+++ src/components/Pages/Landing/Landing.stories.js
@@ -1,10 +1,16 @@
 import { withInfo } from "@storybook/addon-info";
 import { storiesOf } from "@storybook/react";
 import React from "react";
+import { MemoryRouter } from "react-router";
 
 import Landing from "./Landing";
 
 storiesOf("Landing", module)
 
   .add("basic usage", withInfo(`A landing page`)(
-    () => <Landing />));
+    () =>
+      <MemoryRouter>
+        <div style={{ height: "100vh" }}>
+          <Landing />
+        </div>
+      </MemoryRouter>));
